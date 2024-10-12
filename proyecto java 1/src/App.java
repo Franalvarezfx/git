10e5ca9009f1f0fbcc941b0e025e8e7604c82736
Fran Alvarez <franalvarezfx@gmail.com>
2024-10-12 18:29:47 -0500

    primer commit proyecto java 1

--- proyecto java 1/src/App.java
+++ proyecto java 1/src/App.java
@@ -1,5 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hola mundo, por Fran desde java!");
+    
+    /* Ejemplo de 
+     * comentario
+     * de varias
+     * 
+     * lineas
+     */
+
+     System.out.println("esto se ve al medio");
+    // comentario de una sola linea
+
+    System.out.println("esto se ve al final");
+
     }
+    
+
+
 }
